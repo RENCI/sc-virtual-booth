3f80ba0096cdc95ec7632c5b8e9582021f12ded4
Matt Watson <mattwat@gmail.com>
2020-10-07 13:38:23 -0400

    add ncds content to consortia view

--- src/pages/consortia.js
+++ src/pages/consortia.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from "react"
 import { SEO } from '../components/seo'
 import { Hero } from '../components/hero'
-import { Container } from '../components/layout'
+import { Container, Section } from '../components/layout'
 import { Title, Heading, Paragraph } from '../components/typography'
+import { List } from '../components/list'
 
 const ConsortiaPage = () => 
 {  return (
@@ -13,38 +14,55 @@ const ConsortiaPage = () =>
       <Container>
         <Title>Consortia</Title>
 
-        <Heading>South Big Data Hub</Heading>
+        <Section>
+          <Heading>South Big Data Hub</Heading>
 
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-        </Paragraph>
+          </Paragraph>
+        </Section>
 
-        <Heading>National Consortium for Data Sciene</Heading>
+        <Section>
+          <Heading>National Consortium for Data Sciene</Heading>
 
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.
+          <Paragraph>
+            The National Consortium for Data Science (NCDS) is a collaboration of leaders in academia, industry,
+            and government formed to address the data challenges and opportunities of the 21st century.
+            The NCDS helps members take advantage of data in ways that result in new jobs and transformative discoveries.
+          </Paragraph>
 
-        </Paragraph>
+          <Paragraph>
+            We connect diverse communities of data science experts to support a 21st century data-driven economy by:
+          </Paragraph>
 
-        <Heading>BioData Catalyst</Heading>
+          <List bullets="disc"
+            items={ [
+              'Building data science career pathways and creating a data-literate workforce',
+              'Bridging the gap between data scientists in the public and private sectors',
+              'Supporting open and democratized data',
+            ] }
+          />
+        </Section>
 
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+        <Section>
+          <Heading>BioData Catalyst</Heading>
 
-        </Paragraph>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+
+          </Paragraph>
+        </Section>
+
       </Container>
 
 
