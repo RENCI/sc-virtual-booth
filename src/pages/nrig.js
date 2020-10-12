f52c8f2bbcdf1ec459fa650ee4618bb77b40808e
Matt Watson <mattwat@gmail.com>
2020-10-12 13:26:30 -0400

    add fabric content skeleton
    add dummy text

--- src/pages/nrig.js
+++ src/pages/nrig.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react"
 import { SEO } from '../components/seo'
 import { Hero } from '../components/hero'
-import { Container } from '../components/layout'
-import { Title } from '../components/typography'
+import { Container, Section } from '../components/layout'
+import { Title, Heading, Subheading, Paragraph } from '../components/typography'
 import heroBackground from '../images/hero-nrig.jpg'
 
 export default () => {
@@ -21,14 +21,50 @@ export default () => {
       </Hero>
 
       <Container>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <Section>
+          <Heading>FABRIC</Heading>
+          <Subheading>FABRIC is the future</Subheading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Paragraph>
+
+          <Subheading>What is FABRIC?</Subheading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Paragraph>
+
+          <Subheading>Who is behind FABRIC?</Subheading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Paragraph>
+
+          <Subheading>FABRIC is for everyone</Subheading>
+          <Paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Paragraph>
+
+        </Section>
+        
       </Container>
 
     </Fragment>
