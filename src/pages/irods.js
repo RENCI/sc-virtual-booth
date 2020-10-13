598b82fba46de510ef5506b18fbb482142cd9194
Matt Watson <mattwat@gmail.com>
2020-10-13 17:48:19 -0400

    restructure & restyle links

--- src/pages/irods.js
+++ src/pages/irods.js
@@ -9,24 +9,27 @@ import { ExternalLink } from '../components/link'
 import heroBackground from '../images/hero-irods.jpg'
 import { VideoPlayer } from '../components/video-player'
 
-const documents = [
-  { text: 'iRODS Data Management Model', path: 'https://irods.org/images/data_management_model.jpg' },
-  { text: 'Core: Integration Layer', path: 'https://irods.org/images/core_integration_layer.jpg' },
-]
-
-const links = [
-  { text: 'Data Lifecycle', path: 'https://irods.org/images/data_lifecycle_diagram.jpg' },
-  { text: 'Pattern:  Data to  Compute', path: 'https://irods.org/images/pattern_data_to_compute.jpg' },
-  { text: 'Pattern:  Compute to Data', path: 'https://irods.org/images/pattern_compute_to_data.jpg' },
-  { text: 'Pattern:  Synchronization', path: 'https://irods.org/images/pattern_synchronization.jpg' },
-  { text: 'Pattern:  Data Transfer Nodes', path: 'https://irods.org/images/pattern_data_transfer_nodes.png' },
-  { text: 'Capability:  Automated Ingest - Filesystem Scanner', path: 'https://irods.org/images/capability_automated_ingest_filesystem_scanner.jpg' },
-  { text: 'Capability:  Automated Ingest - Landing Zone', path: 'https://irods.org/images/capability_automated_ingest_landing_zone.jpg' },
-  { text: 'Capability:  Storage Tiering', path: 'https://irods.org/images/capability_storage_tiering.jpg' },
-  { text: 'Capability:  Indexing', path: 'https://irods.org/images/capability_indexing.png' },
-  { text: 'Capability:  Publishing', path: 'https://irods.org/images/capability_publishing.png' },
-  { text: 'Capability:  Provenance', path: 'https://irods.org/images/capability_provenance.png' },
-]
+const documents = {
+  general: [
+    { text: 'iRODS Data Management Model', path: 'https://irods.org/images/data_management_model.jpg' },
+    { text: 'Core: Integration Layer', path: 'https://irods.org/images/core_integration_layer.jpg' },
+    { text: 'Data Lifecycle', path: 'https://irods.org/images/data_lifecycle_diagram.jpg' },
+  ],
+  patterns: [
+    { text: 'Data to Compute', path: 'https://irods.org/images/pattern_data_to_compute.jpg' },
+    { text: 'Compute to Data', path: 'https://irods.org/images/pattern_compute_to_data.jpg' },
+    { text: 'Synchronization', path: 'https://irods.org/images/pattern_synchronization.jpg' },
+    { text: 'Data Transfer Nodes', path: 'https://irods.org/images/pattern_data_transfer_nodes.png' },
+  ],
+  capabilities: [
+    { text: 'Automated Ingest - Filesystem Scanner', path: 'https://irods.org/images/capability_automated_ingest_filesystem_scanner.jpg' },
+    { text: 'Automated Ingest - Landing Zone', path: 'https://irods.org/images/capability_automated_ingest_landing_zone.jpg' },
+    { text: 'Storage Tiering', path: 'https://irods.org/images/capability_storage_tiering.jpg' },
+    { text: 'Indexing', path: 'https://irods.org/images/capability_indexing.png' },
+    { text: 'Publishing', path: 'https://irods.org/images/capability_publishing.png' },
+    { text: 'Provenance', path: 'https://irods.org/images/capability_provenance.png' },
+  ],
+}
 
 const videos = [
   { text: 'Overview', path: 'https://www.youtube.com/watch?v=_5eVd3NJ5BU' },
@@ -53,12 +56,31 @@ export default () => {
       <Container>
         <Section>
           <Heading>Documents</Heading>
-          <List items={ documents.map(item => <ExternalLink to={ item.path } key={ item.path }>{ item.text }</ExternalLink>) } />
-        </Section>
 
-        <Section>
-          <Heading>Links</Heading>
-          <List items={ links.map(item => <ExternalLink to={ item.path } key={ item.path }>{ item.text }</ExternalLink>) } />
+          <Subheading>General</Subheading>
+
+          <List items={ documents.general.map(item => (
+            <ExternalLink to={ item.path } key={ item.path } style={{ display: 'flex', justifyContent:'flex-start', alignItems: 'center', gap: '2rem' }}>
+              <img src={ item.path } width="75px" alt="" /> { item.text }
+            </ExternalLink>
+          )) } />
+          
+          <Subheading>Patterns</Subheading>
+
+          <List items={ documents.patterns.map(item => (
+            <ExternalLink to={ item.path } key={ item.path } style={{ display: 'flex', justifyContent:'flex-start', alignItems: 'center', gap: '2rem' }}>
+              <img src={ item.path } width="75px" alt="" /> { item.text }
+            </ExternalLink>
+          )) } />
+
+          <Subheading>Capabilities</Subheading>
+
+          <List items={ documents.capabilities.map(item => (
+            <ExternalLink to={ item.path } key={ item.path } style={{ display: 'flex', justifyContent:'flex-start', alignItems: 'center', gap: '2rem' }}>
+              <img src={ item.path } width="75px" alt="" /> { item.text }
+            </ExternalLink>
+          )) } />
+
         </Section>
 
         <Section>
