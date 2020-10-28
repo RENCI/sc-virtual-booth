a2e9d54e9aa7d7f40d45f3d344ff1ab4a03d6c85
Matt Watson <mattwat@gmail.com>
2020-10-28 14:30:18 -0400

    add drop shadow on video player component

--- src/components/video-player.js
+++ src/components/video-player.js
@@ -2,15 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import ReactPlayer from 'react-player'
 
-const ResponsiveWrapper = styled.div`
+const ResponsiveWrapper = styled.div(({ theme }) => `
   position: relative;
   padding-top: 56.25%;
+  filter: drop-shadow(0 0 4px rgba(0, 0, 0, 0.25));
   & .player {
     position: absolute;
     top: 0;
     left: 0;
   }
-`
+`)
 
 export const VideoPlayer = ({ url }) => {
   return (
