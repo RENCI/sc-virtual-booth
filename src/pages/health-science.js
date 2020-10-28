cfae80a49ad7362e55b74fa90e106fd21bb5538e
Matt Watson <mattwat@gmail.com>
2020-10-28 15:07:06 -0400

    add bdc and dt to health science view

--- src/pages/health-science.js
+++ src/pages/health-science.js
@@ -1,65 +1,124 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useEffect, useState } from "react"
+import Img from 'gatsby-image'
+import { useTheme } from 'styled-components'
+import { useLocation } from "@reach/router"
 import { SEO } from '../components/seo'
-import { Container as Grid, Row, Col } from 'react-grid-system'
 import { Hero } from '../components/hero'
-import { Container } from '../components/layout'
-import { Title, Heading, Paragraph } from '../components/typography'
+import { Container, Section } from '../components/layout'
+import { Title, Heading, Subheading, Paragraph } from '../components/typography'
 import { Button } from '../components/button'
-import heroBackground from '../images/hero-health-science.jpg'
+import { Icon } from '../components/icon'
+import { Link } from '../components/link'
+import { List } from '../components/list'
+import heroBackground from '../images/hero-nrig.jpg'
+import { Container as Grid, Row, Col } from 'react-grid-system'
+import { useLogos } from '../hooks'
+
+
+const BioDataCatalyst = () => {
+  return (
+    <Section>
+      <Heading>BioData Catalyst</Heading>
+      
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Paragraph>
+            
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Paragraph>
+                        
+    </Section>
+  )
+}
+
+const DataTranslator = () => {
+  return (
+    <Section>
+      <Heading>Data Translator</Heading>
+      
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Paragraph>
+            
+    </Section>
+  )
+}
+
+const projectsMenuItems = [
+  { text: 'BioData Catalyst', id: 'bdc', component: <BioDataCatalyst /> },
+  { text: 'Data Translator', id: 'data-translator', component: <DataTranslator /> },
+]
 
 export default () => {
+  const [project, setProject] = useState('bdc')
+  const theme = useTheme()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.hash) {
+      setProject(location.hash.replace('#', ''))
+    }
+  }, [location.hash])
+
   return (
     <Fragment>
       <SEO title="Health Science" />
-
+      
       <Hero backgroundImage={ heroBackground }>
         <Title><span className="highlight">Health Science</span></Title>
         <p className="highlight">
             brief overview. brief overview. brief overview. brief overview. brief overview.
             brief overview. brief overview. brief overview. brief overview. brief overview.
+            brief overview. brief overview. brief overview. brief overview. brief overview.
+            brief overview. brief overview. brief overview. brief overview. brief overview.
         </p>
       </Hero>
-      
+
       <Container>
-        <Button cta>Request a Meeting</Button>
-
-        <br /><br />
-
-        <Paragraph align="center" width="500px">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-        </Paragraph>
-
-        <br /><br /><br />
-
         <Grid fluid>
           <Row>
-            <Col xs={ 12 } md={ 7 } lg={ 6 }>
-              <img src="https://picsum.photos/600/400" alt="placeholder" />
+            <Col xs={ 12 } md={ 3 } style={{ position: 'relative' }}>
+              <List
+                items={ projectsMenuItems.map(item => (
+                  <a
+                    key={ item.id }
+                    href={ `${ location.pathname }#${ item.id }` }
+                    onClick={ () => setProject(item.id) }
+                  >
+                    <Icon icon="renciDash"
+                      size={ 12 } margin="0 0.5rem 0 0"
+                      fill={ project === item.id ? theme.color.primary.main : theme.color.grey.light }
+                    />
+                    { item.text }
+                  </a>
+                )) }
+                style={{ position: 'sticky', marginTop: '3rem', top: '6rem', }}
+              />
             </Col>
-            <Col xs={ 12 } md={ 5 } lg={ 6 }>
-              <Heading>Health Informatics & Biosciences</Heading>
-              <Paragraph>
-                Lorem ipsum dolor sit amet, consectetuer
-                adipiscing elit, sed diam nonummy nibh
-                euismod tincidunt ut laoreet dolore magna
-                aliquam erat volutpat. Ut wisi enim ad minim
-              </Paragraph>
-
-              <Heading>Informatics for Genetic Sequencing</Heading>
-              <Paragraph>
-                Lorem ipsum dolor sit amet, consectetuer
-                adipiscing elit, sed diam nonummy nibh
-                euismod tincidunt ut laoreet dolore magna
-                aliquam erat volutpat. Ut wisi enim ad minim
-              </Paragraph>
+            <Col xs={ 12 } md={ 9 }>
+              { projectsMenuItems.map(item => project === item.id && item.component ) }
             </Col>
           </Row>
         </Grid>
-
+        
       </Container>
+
     </Fragment>
   )
 }
