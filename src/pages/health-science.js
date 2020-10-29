0884d36dca0d30203a84e274d7956c7ec6621293
Matt Watson <mattwat@gmail.com>
2020-10-28 23:03:41 -0400

    add pds

--- src/pages/health-science.js
+++ src/pages/health-science.js
@@ -60,9 +60,34 @@ const DataTranslator = () => {
   )
 }
 
+const PrecisionDosing = () => {
+  return (
+    <Section>
+      <Heading>Precision Dosing</Heading>
+      
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.
+      </Paragraph>
+
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Paragraph>
+            
+    </Section>
+  )
+}
+
 const projectsMenuItems = [
   { text: 'BioData Catalyst', id: 'bdc', component: <BioDataCatalyst /> },
   { text: 'Data Translator', id: 'data-translator', component: <DataTranslator /> },
+  { text: 'Precision Dosing', id: 'pds', component: <PrecisionDosing /> },
 ]
 
 export default () => {
