59229705b8889a8ed95a9f8badf2d0c3e508419d
Matt Watson <mattwat@gmail.com>
2020-11-12 14:20:32 -0500

    update nrig menu link text

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,7 @@ export default [
   { text: 'Home', path: '/' },
   { text: 'Health Science', path: '/health-science' },
   { text: 'Earth Data Science', path: '/eds' },
-  { text: 'NRIG', path: '/nrig' },
+  { text: 'Network Research', path: '/nrig' },
   { text: 'iRODS', path: '/irods' },
   { text: 'Consortia', path: '/consortia' },
   { text: 'COVID-19', path: 'https://covid-19.renci.org/' },
