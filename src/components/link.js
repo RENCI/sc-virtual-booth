78a425c5568fe4b776eba3e6556e6f696e1d8dd6
Matt Watson <mattwat@gmail.com>
2021-10-20 00:13:39 -0400

    add external link markers

--- src/components/link.js
+++ src/components/link.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import { Link as GatsbyLink } from 'gatsby'
 import styled from 'styled-components'
+import { Icon } from './icon'
+
+const ExternalLinkWrapper = styled.span`
+  & > svg {
+    opacity: 0.33;
+  }
+  &:hover svg {
+    opacity: 1.0;
+  }
+`
 
 export const ExternalLink = styled.a.attrs(props => ({
   href: props.to,
@@ -13,6 +23,18 @@ export const Link = ({ to, children, ...props }) => {
   const externalUrlPattern = new RegExp(/^https?:\/\//)
   const externalUrlMatch = externalUrlPattern.exec(to)
   const mailtoMatch = mailtoPattern.exec(to)
-  const LinkComponent = externalUrlMatch || mailtoMatch ? ExternalLink : GatsbyLink
-  return <LinkComponent to={ to } { ...props }>{ children }</LinkComponent>
+  
+  if (externalUrlMatch || mailtoMatch) {
+    return (
+      <ExternalLinkWrapper>
+        <ExternalLink to={ to } { ...props }>{ children }</ExternalLink>{' '}
+        {
+          typeof children === 'string' && (
+            <Icon icon="externalLink" size={ 12 } />
+          )
+        }
+      </ExternalLinkWrapper>
+    )
+  }
+  return <GatsbyLink to={ to } { ...props }>{ children }</GatsbyLink>
 }
