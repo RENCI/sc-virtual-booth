29ec2b9b6a0a5fd2e9d9f41e85b31204340544fa
Matt Watson <mattwat@gmail.com>
2021-10-25 15:57:03 -0400

    add entry animation to section component

--- src/components/layout/section.js
+++ src/components/layout/section.js
@@ -1,5 +1,17 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
+
+const unfade = keyframes`
+  0% {
+    filter: opacity(0.0);
+    transform: translate3d(0, 2px, 0);
+  }
+  100% {
+    filter: opacity(1.0);
+    transform: translate3d(0, 0, 0);
+  }
+`
 
 export const Section = styled.section`
   margin: 2rem 0;
+  animation: ${ unfade } 500ms ease-out forwards;
 `
