3421c0dbe0e32bfbd92f942b50c5eb3b121524bf
Matt Watson <mattwat@gmail.com>
2021-10-26 13:30:28 -0400

    center, enlarge h2

--- src/components/typography.js
+++ src/components/typography.js
@@ -18,6 +18,8 @@ Title.defaultProps = {
 export const Heading = styled.h2(({ theme }) => `
   margin: ${ theme.spacing.extraLarge } 0 ${ theme.spacing.large } 0;
   color: ${ theme.color.primary.dark };
+  text-align: center;
+  font-size: 200%;
 `)
 
 export const Subheading = styled.h3(({ theme }) => `
