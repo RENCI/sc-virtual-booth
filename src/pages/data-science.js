9e416902a2fae8bef22e5858d1aa900e4a30ade4
Suejin <kim.suejin.20@gmail.com>
2021-11-04 11:21:55 -0400

    added header text for data science

--- src/pages/data-science.js
+++ src/pages/data-science.js
@@ -34,6 +34,9 @@ const DataSciencePage = () => {
       
       <Hero backgroundImage={ heroBackground }>
         <Title><span className="highlight">Data Science</span></Title>
+        <p className="highlight">
+        Every sector of society is undergoing a historic transformation driven by big data. RENCI is committed to transforming data into discoveries by partnering with leading universities, government, and the private sector to create tools and technologies that facilitate data access, sharing, analysis, management, and archiving.
+        </p>
       </Hero>
 
       <Container>
